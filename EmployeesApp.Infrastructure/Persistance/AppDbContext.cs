0b46a18b6f34f51326dabcd370ccf089bbf8953f
zhelyazkovse <zhelyazkovse@gmail.com>
2025-06-02 12:47:58 +0200

    Add Entity Framework Core support for SQL Server
    Updated project files to include Microsoft.EntityFrameworkCore.SqlServer package.
    Modified Program.cs to configure AppDbContext with a connection string and changed service registrations to scoped.
    Added DefaultConnection in appsettings.json for local SQL Server setup.
    Introduced AppDbContext class for managing Employee entities.

---
+++ EmployeesApp.Infrastructure/Persistance/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeesApp.Domain.Entities;
+
+namespace EmployeesApp.Infrastructure.Persistance
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- EmployeesApp.Web/Program.cs
+++ EmployeesApp.Web/Program.cs
@@ -2,6 +2,10 @@ using EmployeesApp.Application.Employees.Interfaces;
 using EmployeesApp.Infrastructure.Persistance.Repositories;
 using EmployeesApp.Application.Employees.Services;
 using EmployeesApp.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using EmployeesApp.Infrastructure.Persistance;
+
+
 
 namespace EmployeesApp.Web;
 
@@ -10,9 +14,15 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connString));
+
+
         builder.Services.AddControllersWithViews();
-        builder.Services.AddSingleton<IEmployeeRepository, EmployeeRepository>();
-        builder.Services.AddSingleton<IEmployeeService, EmployeeService>();
+        builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        builder.Services.AddScoped<IEmployeeService, EmployeeService>();
         builder.Services.AddScoped<MyLogServiceFilterAttribute>();
         var app = builder.Build();
         app.MapControllers();
