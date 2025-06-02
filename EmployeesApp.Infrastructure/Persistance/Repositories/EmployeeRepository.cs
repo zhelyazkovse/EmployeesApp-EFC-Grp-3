df88eec17f67d76c425d6b6a6657e217e975d7b8
zhelyazkovse <zhelyazkovse@gmail.com>
2025-06-02 13:38:07 +0200

    Refactor EmployeeRepository to use AppDbContext
    Updated EmployeeRepository to interact with a database context instead of an in-memory list. Removed hardcoded employee instances and modified methods to add, retrieve, and list employees using the database context. This change enhances data persistence and retrieval capabilities.

--- EmployeesApp.Infrastructure/Persistance/Repositories/EmployeeRepository.cs
+++ EmployeesApp.Infrastructure/Persistance/Repositories/EmployeeRepository.cs
@@ -3,55 +3,24 @@ using EmployeesApp.Domain.Entities;
 
 namespace EmployeesApp.Infrastructure.Persistance.Repositories
 {
-    public class EmployeeRepository : IEmployeeRepository
+    public class EmployeeRepository(AppDbContext context) : IEmployeeRepository
     {
-        readonly List<Employee> employees =
-        [
-            new Employee()
-        {
-            Id = 562,
-            Name = "Anders Hejlsberg",
-            Email = "Anders.Hejlsberg@outlook.com",
-        },
-        new Employee()
-        {
-            Id = 62,
-            Name = "Kathleen Dollard",
-            Email = "k.d@outlook.com",
-        },
-        new Employee()
-        {
-            Id = 15662,
-            Name = "Mads Torgersen",
-            Email = "Admin.Torgersen@outlook.com",
-        },
-        new Employee()
-        {
-            Id = 52,
-            Name = "Scott Hanselman",
-            Email = "s.h@outlook.com",
-        },
-        new Employee()
-        {
-            Id = 563,
-            Name = "Jon Skeet",
-            Email = "j.s@outlook.com",
-        },
-        ];
+       
 
         public void Add(Employee employee)
         {
-            employee.Id = employees.Count < 0 ? 1 : employees.Max(e => e.Id) + 1;
-            employees.Add(employee);
+            
+            context.Employees.Add(employee);
+            context.SaveChanges();
         }
 
         //Classic C# syntax for GetAll()
         public Employee[] GetAll()
         {
-            return [.. employees];
+            return [.. context.Employees];
         }
 
-        public Employee? GetById(int id) => employees
-            .SingleOrDefault(e => e.Id == id);
+        public Employee? GetById(int id) => context.Employees
+            .Find(id);
     }
 }
